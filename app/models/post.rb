54780c61a5950b866649810d3473c603d6c88165
marszman <136658905+marszman@users.noreply.github.com>
2023-08-09 22:01:41 +0000

    BB - all except expired posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+  validates(:title, { :presence => true })
+  validates(:body, { :presence => true })
+  validates(:expires_on, { :presence => true })
+  validates(:board_id, { :presence => true })
 end
